833755fbba2a494cff30038d42f819a9be5324ff
YasasPasinduFernando <151065874+YasasPasinduFernando@users.noreply.github.com>
2025-06-12 08:53:47 +0530

    first commit

---
+++ src/test/java/org/agile/petcare/PetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agile.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
